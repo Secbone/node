7b7c4daba582e4b255e3f8577683b1c38d42c19d
secbone <secbone@gmail.com>
2013-07-11 16:17:43 +0800

    new

---
+++ app.js
@@ -0,0 +1,349 @@
+var LINE_SEGMENT = 0;
+var CHAT_MESSAGE = 1;
+var GAME_LOGIC = 2;
+
+var WAITING_TO_START = 0;
+var GAME_START = 1;
+var GAME_OVER = 2;
+var GAME_RESTART = 3;
+
+var conn_sockets = new Array();
+
+var fs = require('fs')
+    , http = require('http')
+    ,url = require('url')
+    ,path = require('path')
+    , socketio = require('socket.io');
+  
+var playerTurn = -1;
+
+var wordsList = ['apple','idea','wisdom','angry','happy','cry','angel','woman','ladygaga','dog','jordan','cat','smile','jump','panda'];
+var currentAnswer = undefined;
+
+var currentGameState = WAITING_TO_START;
+
+var gameOverTimeOut;
+ 
+var server = http.createServer(function(req, res) {
+	
+	var pathname = __dirname + url.parse(req.url).pathname;
+    if (path.extname(pathname) == "") {
+        pathname += "/";
+    }
+    if (pathname.charAt(pathname.length - 1) == "/") {
+        pathname += "index.html";
+    }
+
+    path.exists(pathname, function (exists) {
+        if (exists) {
+            switch (path.extname(pathname)) {
+            case ".html":
+                res.writeHead(200, {
+                    "Content-Type": "text/html"
+                });
+                break;
+            case ".js":
+                res.writeHead(200, {
+                    "Content-Type": "text/javascript"
+                });
+                break;
+            case ".css":
+                res.writeHead(200, {
+                    "Content-Type": "text/css"
+                });
+                break;
+            case ".gif":
+                res.writeHead(200, {
+                    "Content-Type": "image/gif"
+                });
+                break;
+            case ".jpg":
+                res.writeHead(200, {
+                    "Content-Type": "image/jpeg"
+                });
+                break;
+            case ".png":
+                res.writeHead(200, {
+                    "Content-Type": "image/png"
+                });
+                break;
+            default:
+                res.writeHead(200, {
+                    "Content-Type": "application/octet-stream"
+                });
+            }
+
+            fs.readFile(pathname, function (err, data) {
+                res.end(data);
+            });
+        }else {
+            res.writeHead(404, {
+                "Content-Type": "text/html"
+            });
+            res.end("<h1>404 Not Found</h1>");
+        }
+    });
+}).listen(8080, function() {
+    console.log('Listening at: http://localhost:8080');
+    //console.log('--------------------------------------------------------');
+    //console.log('connections : ',socket.manager.connected);
+    //console.log('--------------------------------------------------------');
+});
+ 
+socketio.listen(server).on('connection', function (socket) {
+	
+	//console.log('server : ',server);
+	//console.log('socketio : ',socketio);
+	//console.log('socket : ',socket.manager.sockets.sockets);
+	
+	//for(var key in socket.manager.sockets.sockets){
+	//	socket.manager.sockets.sockets[key].emit('test_r',key);
+	//}
+	socket.on('setname',function(name){
+		socket.set('nickname',name);
+		socket.get('nickname',function(err,name){
+			socket.nickname = name;
+
+			console.log('-----------------'+socket.nickname+'===================');
+			var message = "welcome "+socket.nickname+" joining the party!";
+			    var data = {};
+			    data.dataType = CHAT_MESSAGE;
+			    data.secder = "Server";
+			    data.message = message;
+			    socket.broadcast.emit('welcome',JSON.stringify(data));
+			    
+			    var socket_info = {};
+			    socket_info.id = socket.nickname;
+			    socket_info.ready = 0;
+			    socket_info.score = 0;
+			    conn_sockets.push(socket_info);
+			    
+			    sendPlayerInfo(socket);
+		});
+	});
+	
+
+    
+    
+    //发送游戏状态给所有玩家
+    var gameLogicData = {};
+    gameLogicData.dataType = GAME_LOGIC;
+    gameLogicData.gameState = WAITING_TO_START;
+    socket.emit('message',JSON.stringify(gameLogicData));
+    socket.broadcast.emit('message',JSON.stringify(gameLogicData));
+    
+  
+    
+    socket.on('message', function (msg) {
+        //console.log('Message Received: ', msg);
+        var data = JSON.parse(msg);
+        if(data.dataType == CHAT_MESSAGE){
+			data.sender = socket.nickname;
+			
+		}
+		socket.broadcast.emit("message",JSON.stringify(data));
+		
+		if(data.dataType == CHAT_MESSAGE){
+			//console.log('game_state : '+currentGameState+" Message : "+data.message+" Answer: "+currentAnswer+"------------------------");
+			if(currentGameState == GAME_START && data.message == currentAnswer){
+				if(isPlayer(socket)){
+				}else{
+					getScore(socket);
+					var gameLogicData = {};
+					gameLogicData.dataType = GAME_LOGIC;
+					gameLogicData.gameState = GAME_OVER;
+					gameLogicData.winner = socket.nickname;
+					gameLogicData.answer = currentAnswer;
+					socket.broadcast.emit('message',JSON.stringify(gameLogicData));
+					socket.emit('message',JSON.stringify(gameLogicData));
+					
+					//currentGameState = WAITING_TO_START;
+					nextTurn(socket);
+					
+					clearTimeout(gameOverTimeOut);
+				}
+			}
+		}
+		
+		if(data.dataType == GAME_LOGIC && data.gameState == GAME_RESTART){
+			startGame(socket);
+		}
+		
+		//var displayMessage = socket.nickname + " says: " + msg;
+        //socket.broadcast.emit('message', displayMessage);
+    });
+    
+    socket.on('disconnect',function(){
+		remove_socket(socket.nickname);
+		console.log(socket.nickname+" has disconnect ! ----------------");
+		sendPlayerInfo(socket);
+	});
+	
+	
+	socket.on('getReady',function(data){
+		for(var key in conn_sockets){
+			if(conn_sockets[key].id == socket.nickname){
+				if(data == 'ready'){
+					console.log(socket.nickname+' is ready----------');
+					conn_sockets[key].ready = 1;
+				}else if(data == 'cancel'){
+					console.log(socket.nickname+' is cancel----------');
+					conn_sockets[key].ready = 0;
+				}
+				break;
+			}
+		}
+		if(isAllReady()){
+			clearScore();
+			console.log('------------is all ready-------');
+			playerTurn = (playerTurn + 1) % conn_sockets.length;
+			startGame(socket, playerTurn);
+		}
+		sendPlayerInfo(socket);
+		console.log('---------------------ready ?????????----- : ');
+	});
+});
+
+function startGame(socket){
+	
+	console.log('--------------Game Start!------------');
+	var turning = arguments[1] ? arguments[1] : 0;
+	
+	var answerIndex = Math.floor(Math.random() * wordsList.length);
+	currentAnswer = wordsList[answerIndex];
+	
+	sendPlayerInfo(socket);
+	
+	var gameLogicData1 = {};
+	gameLogicData1.dataType = GAME_LOGIC;
+	gameLogicData1.gameState = GAME_START;
+	gameLogicData1.isPlayerTurn = false;
+	socket.emit('message',JSON.stringify(gameLogicData1));
+	socket.broadcast.emit('message',JSON.stringify(gameLogicData1));
+	
+	for(var key in socket.manager.sockets.sockets){
+		if(key == conn_sockets[turning].id){
+			socket.manager.sockets.sockets[key].broadcast.emit('message',JSON.stringify(gameLogicData1));
+			gameLogicData1.isPlayerTurn = true;
+			gameLogicData1.answer = currentAnswer;
+			socket.manager.sockets.sockets[key].emit('message',JSON.stringify(gameLogicData1));
+			break;
+		}
+	}
+	
+	//依次回答
+	var index = 0;
+	//--------------------------------------------to be continue !
+	
+	gameOverTimeOut = setTimeout(function(){
+		var gameLogicData = {};
+		gameLogicData.dataType = GAME_LOGIC;
+		gameLogicData.gameState = GAME_OVER;
+		gameLogicData.winner = "No one";
+		gameLogicData.answer = currentAnswer;
+		socket.emit('message',JSON.stringify(gameLogicData));
+		socket.broadcast.emit('message',JSON.stringify(gameLogicData));
+		
+		nextTurn(socket);
+		
+		//currentGameState = WAITING_TO_START;
+	},60*1000);
+	
+	currentGameState = GAME_START;
+}
+
+function remove_socket(id){
+	for(var index in conn_sockets){
+		if(conn_sockets[index].id == id){
+			conn_sockets.splice(index,1);
+			break;
+		}
+	}
+}
+
+function isAllReady(){
+	console.log('-----------------check ready---------------');
+	console.log('-------check : ',conn_sockets);
+	var isReady = 1;
+	for(var key in conn_sockets){
+		isReady *= conn_sockets[key].ready;
+	}
+	return isReady;
+}
+
+function clearReady(){
+	for(var key in conn_sockets){
+		conn_sockets[key].ready = 0;
+	}
+}
+
+function clearScore(){
+	for(var key in conn_sockets){
+		conn_sockets[key].score = 0;
+	}
+}
+
+function nextTurn(socket){
+	playerTurn = (playerTurn + 1) % conn_sockets.length;
+	var data={};
+	if(playerTurn == 0){
+		playerTurn = -1;
+		data.msg = 'Game Over! please ready!';
+		data.isOver = true;
+		clearReady();
+		currentGameState = WAITING_TO_START;
+		sendPlayerInfo(socket);
+	}else{
+		data.msg = '5 second to next!';
+		data.isOver = false;
+		console.log('--------------------next time out---------------');
+		var nextTimeout = setTimeout(startGame, 5*1000, socket, playerTurn);
+	}
+	socket.emit('endmsg',JSON.stringify(data));
+	socket.broadcast.emit('endmsg',JSON.stringify(data));
+}
+
+function sendPlayerInfo(socket){
+	var data = {};
+	data.playerinfo = conn_sockets;
+	data.playturn = playerTurn;
+	socket.emit('playerinfo',JSON.stringify(data));
+	socket.broadcast.emit('playerinfo',JSON.stringify(data));
+}
+
+function getScore(socket){
+	for(var index in conn_sockets){
+		if(index == playerTurn){
+			conn_sockets[index].score += 2;
+		}
+		if(conn_sockets[index].id == socket.nickname){
+			conn_sockets[index].score += 1;
+		}
+	}
+	sendPlayerInfo(socket);
+}
+
+function isPlayer(socket){
+	for(var index in conn_sockets){
+		if(conn_sockets[index].id == socket.nickname){
+			if(index == playerTurn)
+				return true;
+			return false;
+		}
+	}
+}
+
+//重写setTimeout函数
+var _st = setTimeout;
+//fRef 是test函数,mDelay是时间
+setTimeout = function(fRef, mDelay) {
+   if(typeof fRef == 'function'){ 
+       var argu = Array.prototype.slice.call(arguments,2);
+       var f = (
+            function(){
+                fRef.apply(null, argu);
+            }); 
+       return _st(f, mDelay);
+    }
+    return _st(fRef,mDelay);
+}
---
+++ html5.js
@@ -0,0 +1,277 @@
+var websocketGame = {
+	//指示当前是否正在进行绘图
+	isDrawing : false,
+	isTurnToDraw : false,
+	//绘制下一条线的起点
+	startX : 0,
+	startY : 0,
+	color : "#000",
+	thickness : 1,
+	//常量
+	LINE_SEGMENT : 0,
+	CHAT_MESSAGE : 1,
+	GAME_LOGIC : 2,
+	//游戏逻辑状态常量
+	WAITING_TO_START : 0,
+	GAME_START : 1,
+	GAME_OVER : 2,
+	GAME_RESTART : 3,
+}
+var timer;
+var canvas = document.getElementById('drawing-pad');
+var ctx = canvas.getContext('2d');
+$(function(){
+	var iosocket = io.connect();
+
+	iosocket.on('connect', function () {
+		var name = prompt("请输入姓名：");
+		var name = name ? name : "某某";
+		iosocket.emit('setname',name);
+		var html = $('#incomingChatMessages').text()+"\r";
+		$('#incomingChatMessages').text(html+'Connected');
+		$('#incomingChatMessages').scrollTop=$('#incomingChatMessages').scrollHeight;
+		iosocket.on('welcome',function(data){
+			//console.log(data);
+			var data = JSON.parse(data);
+			var html = $('#incomingChatMessages').text()+"\r";
+			$('#incomingChatMessages').text(html +data.message);
+			$('#incomingChatMessages').scrollTop=$('#incomingChatMessages').scrollHeight;
+		})
+
+		iosocket.on('message', function(message) {
+			//console.log(message);
+			var data = JSON.parse(message);
+			//收到聊天信息
+			if(data.dataType == websocketGame.CHAT_MESSAGE){
+				var html = $('#incomingChatMessages').text()+"\r";
+				$('#incomingChatMessages').text(html +data.sender+" said: "+data.message);
+				$('#incomingChatMessages').scrollTop=$('#incomingChatMessages').scrollHeight; 
+			}else if(data.dataType == websocketGame.LINE_SEGMENT){
+				//同步其他玩家的线条
+				drawLine(ctx, data.startX, data.startY, data.endX, data.endY, data.thickness,data.color);
+			}else if(data.dataType == websocketGame.GAME_LOGIC){
+				if(data.gameState == websocketGame.GAME_OVER){
+					//单个游戏结束
+					websocketGame.isTurnToDraw = false;
+					$(".color-pad").hide();
+					$(".game-info").show();
+					var html = $('#incomingChatMessages').text()+"\r";
+					$('#incomingChatMessages').text(html+data.winner+" wins! The answer is '"+data.answer+"' !");
+					//$('#ready').show();
+					$('#incomingChatMessages').scrollTop=$('#incomingChatMessages').scrollHeight;
+				}
+				if(data.gameState == websocketGame.GAME_START){
+					//游戏开始
+					//console.log('start');
+					showTime(60);
+					//清除Canvas
+					canvas.width = canvas.width;
+					websocketGame.color = "#000";
+					websocketGame.thickness = 1;
+					$('#ready').hide();
+					$('#cancel').hide();
+					$('#incomingChatMessages').text("");
+					
+					//console.log(data.isPlayerTurn);
+					
+					if(data.isPlayerTurn){
+						//当前玩家绘画
+						websocketGame.isTurnToDraw = true;
+						$(".game-info").hide();
+						$(".color-pad").show();
+						$('#drawing-pad').removeClass('n_drawing');
+						$('#drawing-pad').addClass('drawing');
+						var html = $('#incomingChatMessages').text()+"\r";
+						$('#incomingChatMessages').text(html+"Your turn to draw,pleas draw '"+data.answer+"' ");
+						$('#incomingChatMessages').scrollTop=$('#incomingChatMessages').scrollHeight;
+					}else{
+						//其他玩家绘画
+						var html = $('#incomingChatMessages').text()+"\r";
+						$('#incomingChatMessages').text(html+"Game start,Get ready,you hava 1 min to guess");
+						$('#incomingChatMessages').scrollTop=$('#incomingChatMessages').scrollHeight;
+						$('#drawing-pad').removeClass('drawing');
+						$('#drawing-pad').addClass('n_drawing');
+					}
+				}
+			}
+		});
+		//失去连接
+		iosocket.on('disconnect', function() {
+			var html = $('#incomingChatMessages').text()+"\r";
+			$('#incomingChatMessages').text(html+'Disconnected');
+			$('#incomingChatMessages').scrollTop=$('#incomingChatMessages').scrollHeight;
+		});
+		iosocket.on('socketsss',function(data){
+			var connections = JSON.parse(data);
+			var length = 0;
+			for( var key in connections){
+				//console.log("-------------key : "+key+" value : "+connections[key]);
+				length++;
+			}
+			//console.log("length : "+length);
+		});
+		
+		iosocket.on('test_r',function(data){
+			//console.log("my id is : "+data);
+		});
+		//整轮游戏结束
+		iosocket.on('endmsg',function(data){
+			var data = JSON.parse(data);
+			var html = $('#incomingChatMessages').text()+"\r";
+			$('#incomingChatMessages').text(html+data.msg);
+			$('#incomingChatMessages').scrollTop=$('#incomingChatMessages').scrollHeight;
+			if(data.isOver){
+				websocketGame.isTurnToDraw = false;
+				$('#drawing-pad').removeClass('drawing');
+				$('#drawing-pad').addClass('n_drawing');
+				$('#ready').show();
+				clearTimeout(timer);
+				$('.time-pad').html("");
+			}else{
+				//5秒延时
+				showTime(5);
+			}
+		});
+		iosocket.on('playerinfo', function(data){
+			var data = JSON.parse(data);
+			$(".player-box").html('');
+			for(var key in data.playerinfo){
+				var html = $(".player-box").html();
+				if(key == data.playturn){
+					$(".player-box").html(html + '<div class="player-info"><div class="player-img player-drawing"></div><div class="player-name">'+data.playerinfo[key].id+'</div><div class="player-score">'+data.playerinfo[key].score+'</div></div>');
+				}else if(data.playerinfo[key].ready == 1){
+					$(".player-box").html(html + '<div class="player-info"><div class="player-img player-ready"></div><div class="player-name">'+data.playerinfo[key].id+'</div><div class="player-score">'+data.playerinfo[key].score+'</div></div>');
+				}else{
+					$(".player-box").html(html + '<div class="player-info"><div class="player-img"></div><div class="player-name">'+data.playerinfo[key].id+'</div><div class="player-score">'+data.playerinfo[key].score+'</div></div>');
+				}
+			}
+		});
+		
+	});
+	//发送聊天信息
+	$("#send").click(sendMessage);
+	$('#outgoingChatMessage').keypress(function(event) {
+		if(event.which == 13) {
+			event.preventDefault();
+			sendMessage();
+			/*iosocket.send($('#outgoingChatMessage').val());
+			$('#incomingChatMessages').append($('<li></li>').text($('#outgoingChatMessage').val()));
+			$('#outgoingChatMessage').val('');
+			*/
+		}
+	});
+	
+	function sendMessage(){
+		var message = $('#outgoingChatMessage').val();
+		var data = {};
+		data.dataType = websocketGame.CHAT_MESSAGE;
+		data.message = message;
+		iosocket.send(JSON.stringify(data));
+		var html = $('#incomingChatMessages').text()+"\r";
+		$('#incomingChatMessages').text(html+$('#outgoingChatMessage').val());
+		$('#incomingChatMessages').scrollTop=$('#incomingChatMessages').scrollHeight;
+		$('#outgoingChatMessage').val('');
+	}
+	
+	//canvas绘图的逻辑
+	$('#drawing-pad').mousedown(function(e){
+		var canvasPosition = $(this).offset();
+		var mouseX = (e.pageX - canvasPosition.left)  || 0;
+		var mouseY = (e.pageY - canvasPosition.top) || 0;
+		websocketGame.startX = mouseX;
+		websocketGame.startY = mouseY;
+		websocketGame.isDrawing = true;
+	});
+	
+	$('#drawing-pad').mousemove(function(e){
+		if( websocketGame.isTurnToDraw  && websocketGame.isDrawing){
+			var canvasPosition = $(this).offset();
+			var mouseX = (e.pageX - canvasPosition.left) || 0;
+			var mouseY = (e.pageY - canvasPosition.top) || 0;
+			
+			if(!(mouseX == websocketGame.startX && mouseY == websocketGame.startY)){
+				drawLine(ctx, websocketGame.startX, websocketGame.startY, mouseX, mouseY, websocketGame.thickness,websocketGame.color);
+				//发送线段数据到服务器
+				var data = {};
+				data.dataType = websocketGame.LINE_SEGMENT;
+				data.startX = websocketGame.startX;
+				data.startY = websocketGame.startY;
+				data.endX = mouseX;
+				data.endY = mouseY;
+				data.color = websocketGame.color;
+				data.thickness = websocketGame.thickness;
+				iosocket.send(JSON.stringify(data));
+				websocketGame.startX = mouseX;
+				websocketGame.startY = mouseY; 
+			}
+		}
+	});
+	
+	$('#drawing-pad').mouseup(function(e){
+		websocketGame.isDrawing = false;
+	});
+	
+	$('#ready').click(function(){
+		iosocket.emit('getReady','ready');
+		$('#ready').hide();
+		$('#cancel').show();
+	});
+	
+	$('#cancel').click(function(){
+		iosocket.emit('getReady','cancel');
+		$('#ready').show();
+		$('#cancel').hide();
+	});
+	
+	$(".color-item").click(function(){
+		websocketGame.color = $(this).attr("color-value");
+		if(websocketGame.color == "#fff"){
+			websocketGame.thickness = 8;
+			$('#drawing-pad').removeClass('drawing');
+			$('#drawing-pad').addClass('rubber');
+		}else{
+			websocketGame.thickness = 1;
+			$('#drawing-pad').removeClass('rubber');
+			$('#drawing-pad').addClass('drawing');
+		}
+		//console.log("color : "+websocketGame.color);
+		$(this).addClass("c-selected").siblings().removeClass("c-selected");
+	});
+});
+function drawLine(ctx, x1, y1, x2, y2, thickness, color){
+	ctx.beginPath();
+	ctx.moveTo(x1, y1);
+	ctx.lineTo(x2, y2);
+	ctx.lineWidth = thickness;
+	ctx.strokeStyle = color;
+	ctx.stroke();
+}
+function showTime(time){
+	clearTimeout(timer);
+	if(time < 0){
+		$(".time-pad").html("");
+	}else{
+		if(time <= 10){
+			$(".time-pad").html('<span style="color : red">'+time+'</span>');
+		}else{
+			$(".time-pad").html(time);
+		}
+		time = time -1;
+		timer = setTimeout(showTime,1000,time);
+	}
+}
+
+//重写setTimeout函数
+var _st = setTimeout;
+//fRef 是test函数,mDelay是时间
+setTimeout = function(fRef, mDelay) {
+   if(typeof fRef == 'function'){ 
+       var argu = Array.prototype.slice.call(arguments,2);
+       var f = (
+            function(){
+                fRef.apply(null, argu);
+            }); 
+       return _st(f, mDelay);
+    }
+    return _st(fRef,mDelay);
+}
